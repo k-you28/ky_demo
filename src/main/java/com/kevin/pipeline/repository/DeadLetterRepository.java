8373f0bcb50d73cda2e8af89c1c43f4dc73ec7c1
kyou <kyou24@wisc.edu>
2026-02-24 19:43:41 -0600

    Remove legacy pipeline and scope GitHub Actions to job app

--- src/main/java/com/kevin/pipeline/repository/DeadLetterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kevin.pipeline.repository;
-
-import com.kevin.pipeline.entity.DeadLetterEvent;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DeadLetterRepository
-        extends JpaRepository<DeadLetterEvent, String> {
-}
-
