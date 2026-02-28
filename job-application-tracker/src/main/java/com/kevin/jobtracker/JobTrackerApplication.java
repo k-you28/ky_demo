ab49fac79bf87edd932a719fdc45e01cf89688dc
kyou <kyou24@wisc.edu>
2026-02-28 14:18:40 -0600

    Breaking dev branch test

--- job-application-tracker/src/main/java/com/kevin/jobtracker/JobTrackerApplication.java
+++ job-application-tracker/src/main/java/com/kevin/jobtracker/JobTrackerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JobTrackerApplication {
 	public static void main(String[] args) {
-		//SpringApplication.run(JobTrackerApplication.class, args);
+		SpringApplication.run(JobTrackerApplication.class, args);
 		System.out.println("Hello World");
 	}
 }
