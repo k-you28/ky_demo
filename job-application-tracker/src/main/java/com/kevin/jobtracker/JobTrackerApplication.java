0234a0697c81b97d43454faf926ecf1d98b5c07a
kyou <kyou24@wisc.edu>
2026-02-28 09:16:38 -0600

    BAD COMMIT, SHOULD FAIL ON PUSH

--- job-application-tracker/src/main/java/com/kevin/jobtracker/JobTrackerApplication.java
+++ job-application-tracker/src/main/java/com/kevin/jobtracker/JobTrackerApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JobTrackerApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(JobTrackerApplication.class, args);
+		//SpringApplication.run(JobTrackerApplication.class, args);
+		System.out.println("Hello World");
 	}
 }
